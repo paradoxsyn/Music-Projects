8b31901ce3fce58004a549388cffc2d67f8c6937
paradox <paradoxsyn@gmail.com>
2016-11-12 02:19:43 -0500

    Feefth commit

--- Losu/app/src/main/java/com/example/rabithole/losu/Ryze.java
+++ Losu/app/src/main/java/com/example/rabithole/losu/Ryze.java
@@ -25,7 +25,7 @@ public class Ryze extends AppCompatActivity {
     Button r3;
     Button r4;
 
-    public Runnable runnable = new Runnable() {
+    public Runnable runone = new Runnable() {
         @Override
         public void run() {
             r1 = (Button) findViewById(R.id.ryze_1);
@@ -33,47 +33,83 @@ public class Ryze extends AppCompatActivity {
             r3 = (Button) findViewById(R.id.ryze_3);
             r4 = (Button) findViewById(R.id.ryze_4);
 
-            r1.setBackgroundColor(Color.RED);
-            r2.setBackgroundColor(Color.RED);
-            r3.setBackgroundColor(Color.RED);
-            r4.setBackgroundColor(Color.RED);
-
-
-
             if(started){
                 r1.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         r1.setBackgroundColor(Color.GREEN);
-                        handler.postDelayed(runnable, 6000);
+                        r1.setClickable(false);
+                        handler.postDelayed(runone, 6000);
                     }
-                }); handler.removeCallbacks(runnable);
-                r2.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        r2.setBackgroundColor(Color.GREEN);
-                        r1.setBackgroundColor(Color.RED);
-                        handler.postDelayed(runnable, 13000);
-                    }
-                });handler.removeCallbacks(runnable);
-                r3.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        r3.setBackgroundColor(Color.GREEN);
-                        r1.setBackgroundColor(Color.RED);
-                        handler.postDelayed(runnable, 2250);
-
-                    }
-                });handler.removeCallbacks(runnable);
-                r4.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        r4.setBackgroundColor(Color.GREEN);
-                        handler.postDelayed(runnable, 120000);
-                    }
-                });
-                //start();
+                });r1.setBackgroundColor(Color.RED);
+                r1.setClickable(true);
             }
+        }
+    };
+
+    public Runnable runtwo = new Runnable() {
+        @Override
+        public void run() {
+            r1 = (Button) findViewById(R.id.ryze_1);
+            r2 = (Button) findViewById(R.id.ryze_2);
+            r3 = (Button) findViewById(R.id.ryze_3);
+            r4 = (Button) findViewById(R.id.ryze_4);
+
+            r2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    r2.setBackgroundColor(Color.GREEN);
+                    r1.setBackgroundColor(Color.RED);
+                    r1.setClickable(true);
+                    handler.removeCallbacks(runone);
+                    r2.setClickable(false);
+                    handler.postDelayed(runtwo, 13000);
+                }
+            });r2.setBackgroundColor(Color.RED);
+            r2.setClickable(true);
+
+        }
+    };
+
+    public Runnable runthree = new Runnable() {
+        @Override
+        public void run() {
+            r1 = (Button) findViewById(R.id.ryze_1);
+            r2 = (Button) findViewById(R.id.ryze_2);
+            r3 = (Button) findViewById(R.id.ryze_3);
+            r4 = (Button) findViewById(R.id.ryze_4);
+
+            r3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    r3.setBackgroundColor(Color.GREEN);
+                    r1.setBackgroundColor(Color.RED);
+                    r1.setClickable(true);
+                    handler.removeCallbacks(runone);
+                    r3.setClickable(false);
+                    handler.postDelayed(runthree, 2250);
+                }
+            });r3.setBackgroundColor(Color.RED);
+            r3.setClickable(true);
+
+        }
+    };
+
+    public Runnable runfour = new Runnable() {
+        @Override
+        public void run() {
+            r2 = (Button) findViewById(R.id.ryze_2);
+            r3 = (Button) findViewById(R.id.ryze_3);
+            r4 = (Button) findViewById(R.id.ryze_4);
+
+            r4.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    r4.setBackgroundColor(Color.GREEN);
+                    handler.postDelayed(runfour, 120000);
+                }
+            });
+            //start();
         }
     };
 
@@ -85,13 +121,30 @@ public class Ryze extends AppCompatActivity {
         r3.setBackgroundColor(Color.BLUE);
         r4.setBackgroundColor(Color.BLUE);
 
-        handler.removeCallbacks(runnable);
+        handler.removeCallbacks(runone);
+        handler.removeCallbacks(runtwo);
+        handler.removeCallbacks(runthree);
+        handler.removeCallbacks(runfour);
+
+        r1.setClickable(false);
+        r2.setClickable(false);
+        r3.setClickable(false);
+        r4.setClickable(false);
+
     }
 
     public void start() {
         started = true;
 
-        handler.post(runnable);
+        r1.setBackgroundColor(Color.RED);
+        r2.setBackgroundColor(Color.RED);
+        r3.setBackgroundColor(Color.RED);
+        r4.setBackgroundColor(Color.RED);
+
+        handler.post(runone);
+        handler.post(runtwo);
+        handler.post(runthree);
+        handler.post(runfour);
     }
 
     @Override
